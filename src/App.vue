860e55dc87915f027aacc72a01c2cebfed4e3ad0
pjdtorres <paulo.torres@ipcb.pt>
2024-03-21 16:45:51 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Formulario from "./components/Formulario.vue";
+import useClima from "./composables/useClima";
+import Clima from "./components/Clima.vue";
+import Spinner from "./components/Spinner.vue";
+import Alerta from "./components/Alerta.vue";
+
+const { obtenerClima, clima, cargando, mostrarClima, error } = useClima();
+</script>
+
+<template>
+  <h1 class="titulo">Procura de Clima</h1>
+  <div class="contenedor buscador-clima">
+    <Formulario @obtener-clima="obtenerClima" />
+
+    <Spinner v-if="cargando" />
+    <Alerta v-if="error">{{ error }}</Alerta>
+    <Clima v-if="mostrarClima" :clima="clima" />
+  </div>
+</template>
